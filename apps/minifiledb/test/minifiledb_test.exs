c3b5868572bbff75ad03bebbaa95b83f44838e44
Khanh Nguyen <44149581+Kn99HN@users.noreply.github.com>
2021-11-10 11:54:30 -0500

    Concurrent read/write tests (#4)
    * Add some more tests to concurrently write
    
    * Fix tests

--- apps/minifiledb/test/minifiledb_test.exs
+++ apps/minifiledb/test/minifiledb_test.exs
@@ -18,16 +18,52 @@ defmodule MinifiledbTest do
     Minifiledb.terminate()
   end
 
-  test "write and compact" do
-    Minifiledb.init()
-    1..26
-    |> Enum.to_list()
+  defp generate(seq) do
+    seq |> Enum.to_list()
     |> Enum.map(fn x -> %{ key: x, val: "Hello.#{x}"} end) 
     |> Enum.map(fn x -> Minifiledb.write(x.key, x.val) end)
+
+  end
+
+  test "write and compact" do
+    Minifiledb.init()
+    generate(1..26)
 
     assert Minifiledb.read(1) == "Hello.1"
     assert Minifiledb.read(6) == "Hello.6"
     assert Minifiledb.read(26) == "Hello.26"
+
+    Minifiledb.terminate()
+  end
+
+  defp read do
+    assert Minifiledb.read(1) == "Hello.1"
+    assert Minifiledb.read(2) == "Hello.2"
+  end
+
+  test "concurrent reads" do
+    Minifiledb.init()
+
+    generate(1..10)
+
+    tasks = [Task.async(fn -> read end), Task.async(fn -> read end)]
+    Task.yield_many(tasks)
+
+    Minifiledb.terminate()
+  end
+
+  def write(seq) do
+    generate(seq)
+
+    assert Minifiledb.read(16) == "Hello.16"
+  end
+
+  test "concurrent writes" do
+    Minifiledb.init()
+
+    tasks = [Task.async(fn -> write(1..16) end), Task.async(fn -> write(5..21) end)]
+
+    Task.yield_many(tasks)
 
     Minifiledb.terminate()
   end
